8d5e41dd4905681ba0255af02655480da038b857
drfailov <fsoft-p@narod.ru>
2017-01-23 17:17:04 +0200

    Initial commit

---
+++ FP Converter/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace FP_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.labelReports = new System.Windows.Forms.Label();
+            this.labelErrors = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonDelSelected = new System.Windows.Forms.Button();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.textBoxRequest = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(10, 32);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(560, 441);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonStop);
+            this.panel1.Controls.Add(this.labelReports);
+            this.panel1.Controls.Add(this.labelErrors);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.buttonDelSelected);
+            this.panel1.Controls.Add(this.buttonGo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(570, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(210, 463);
+            this.panel1.TabIndex = 1;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(91, 3);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(95, 39);
+            this.buttonStop.TabIndex = 6;
+            this.buttonStop.Text = "STOP";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // labelReports
+            // 
+            this.labelReports.ForeColor = System.Drawing.Color.DarkGreen;
+            this.labelReports.Location = new System.Drawing.Point(6, 383);
+            this.labelReports.Name = "labelReports";
+            this.labelReports.Size = new System.Drawing.Size(159, 33);
+            this.labelReports.TabIndex = 5;
+            this.labelReports.Text = "No reports";
+            this.labelReports.Click += new System.EventHandler(this.labelReports_Click);
+            // 
+            // labelErrors
+            // 
+            this.labelErrors.ForeColor = System.Drawing.Color.Maroon;
+            this.labelErrors.Location = new System.Drawing.Point(7, 339);
+            this.labelErrors.Name = "labelErrors";
+            this.labelErrors.Size = new System.Drawing.Size(159, 33);
+            this.labelErrors.TabIndex = 4;
+            this.labelErrors.Text = "No errors";
+            this.labelErrors.Click += new System.EventHandler(this.labelErrors_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 196);
+            this.label2.TabIndex = 3;
+            this.label2.Text = resources.GetString("label2.Text");
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(7, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 46);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Old files will be owerwriten";
+            // 
+            // buttonDelSelected
+            // 
+            this.buttonDelSelected.Location = new System.Drawing.Point(3, 430);
+            this.buttonDelSelected.Name = "buttonDelSelected";
+            this.buttonDelSelected.Size = new System.Drawing.Size(112, 26);
+            this.buttonDelSelected.TabIndex = 1;
+            this.buttonDelSelected.Text = "Del selected";
+            this.buttonDelSelected.UseVisualStyleBackColor = true;
+            this.buttonDelSelected.Click += new System.EventHandler(this.buttonDelSelected_Click);
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Location = new System.Drawing.Point(6, 3);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(75, 39);
+            this.buttonGo.TabIndex = 0;
+            this.buttonGo.Text = "GO";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // textBoxRequest
+            // 
+            this.textBoxRequest.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBoxRequest.Location = new System.Drawing.Point(10, 10);
+            this.textBoxRequest.Name = "textBoxRequest";
+            this.textBoxRequest.Size = new System.Drawing.Size(560, 22);
+            this.textBoxRequest.TabIndex = 2;
+            this.textBoxRequest.Text = "ffmpeg.exe -i \"(INFILE)\" -c:v libx265 -preset slow -crf 30 -c:a aac -b:a 160k \"(O" +
+    "UTFILE)\"";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 483);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBoxRequest);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Text = "FP Converter";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.Button buttonDelSelected;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelErrors;
+        private System.Windows.Forms.Label labelReports;
+        private System.Windows.Forms.TextBox textBoxRequest;
+        private System.Windows.Forms.Button buttonStop;
+    }
+}
+
---
+++ FP Converter/Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FP_Converter
+{
+    public partial class Form1 : Form
+    {
+        //ffmpeg -i input -c:v libx265 -preset medium -crf 28 -c:a aac -b:a 128k output.mp4
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        string tmpfile = Directory.GetCurrentDirectory() + "\\tmp.mp4";
+        string recycledir = Directory.GetCurrentDirectory() + "\\ConvertedRecycle";
+        string errors = "";
+        string reports = "";
+        Process process = null;
+        bool stop = false;
+                
+
+        public Form1()
+        {
+            AllocConsole();
+            InitializeComponent();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            //MessageBox.Show(Directory.GetCurrentDirectory());
+        }
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            //listBox1.Enabled = false;
+            stop = false;
+            buttonDelSelected.Enabled = false;
+            buttonGo.Enabled = false;
+            textBoxRequest.Enabled = false;
+            buttonStop.Enabled = true;
+
+            labelErrors.Text = "No errors.";
+            labelReports.Text = "No reports.";
+            reports = "";
+            errors = "";
+
+            for (int i = 0; i < listBox1.Items.Count && !stop; i++)
+            {
+                String videofile = listBox1.Items[i].ToString();
+                Console.WriteLine("---------------------------------------------------------------------------------------");
+                Console.WriteLine("       ENCODING  " + i + " OF " + listBox1.Items.Count + "     FILE: " + videofile);
+                Console.WriteLine("---------------------------------------------------------------------------------------");
+                Console.WriteLine("Removing: " + tmpfile);
+                try
+                {
+                    File.Delete(tmpfile);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error: Cannot delete temp file!");
+                    errors += "Error: Cannot delete temp file!   for file = " + videofile + "\n";
+                    int numErrors = errors.Split('\n').Length;
+                    labelErrors.Text = numErrors + " errors!";
+                    continue;
+                }
+                //"ffmpeg.exe -i \"" + videofile + "\" -c:v libx265 -preset slow -crf 30 -c:a aac -b:a 160k \"" + tmpfile + "\"";
+                string request = textBoxRequest.Text;
+                if (request.Equals(""))
+                {
+                    Console.WriteLine("Error: no request field provided!");
+                    continue;
+                }
+                if (!request.Contains("(INFILE)"))
+                {
+                    Console.WriteLine("Error: no (INFILE) placeholder provided!");
+                    continue;
+                }
+                if (!request.Contains("(OUTFILE)"))
+                {
+                    Console.WriteLine("Error: no (OUTFILE) placeholder provided!");
+                    continue;
+                }
+                Console.WriteLine("Request OK: " + request);
+                request = request.Replace("(INFILE)", videofile);
+                request = request.Replace("(OUTFILE)", tmpfile);
+                cmd(request);
+                if (stop)
+                {
+                    Console.WriteLine("Stopped!");
+                    return;
+                }
+                bool exists = false;
+                long filesize = 0;
+                bool success = true;
+                String error = "none";
+                try
+                {
+                    exists = File.Exists(tmpfile);
+                    filesize = new FileInfo(tmpfile).Length;
+                }
+                catch (Exception exc)
+                {
+                    success = false;
+                    error = e.ToString();
+                }
+                if (success && exists && filesize > 1000)
+                {
+                    Console.WriteLine("File successfully converted: " + videofile); 
+                    Console.WriteLine("Wait while file unlocks...");
+                    while (IsFileLocked(new FileInfo(videofile)))
+                    {
+                        Console.WriteLine("Kill FFmpeg...");
+                        Process[] proc = Process.GetProcessesByName("ffmpeg.exe");
+                        if(proc.Length > 0)
+                            proc[0].Kill();
+                        Thread.Sleep(1000);
+                        Application.DoEvents();
+                    }
+
+                    Console.WriteLine("Replacing original file...");
+                    try
+                    {
+                        Console.WriteLine("Moving: " + videofile);
+                        Console.WriteLine("To: " + recycledir);
+                        Directory.CreateDirectory(recycledir);
+                        string filerecyclepath = recycledir + Path.DirectorySeparatorChar + Path.GetFileName(videofile);
+                        while (File.Exists(filerecyclepath))
+                            filerecyclepath += "1";
+                        File.Move(videofile, filerecyclepath);
+                        Console.WriteLine("Moved.");
+
+                        string path = Path.GetDirectoryName(videofile) + Path.DirectorySeparatorChar;
+                        string extension = ".mp4";
+                        string filename = Path.GetFileNameWithoutExtension(videofile);
+                        string newfilepath = path + filename + extension;
+                        Console.WriteLine("Moving: " + tmpfile);
+                        Console.WriteLine("To: " + newfilepath);
+                        File.Move(tmpfile, newfilepath);
+                        Console.WriteLine("Moved.");
+
+                        reports += "Size: " + filesize / 1000 + "kb    File: " + videofile + "\n";
+                        int numLines = reports.Split('\n').Length;
+                        labelReports.Text = numLines + " reports";
+
+                    }
+                    catch(Exception exx){
+                        error = exx.ToString();
+                        Console.WriteLine("Error moving: " + exx);
+                        Console.WriteLine("!!!!!!!!!!!!!!!!!1 ERROR!!!!");
+                        Console.WriteLine("success = " + success + "  exists = " + exists + "  size = " + "  errormoving = " + error + "  file = " + videofile);
+                        errors += "success = " + success + "  exists = " + exists + "  size = " + "  error = " + error + "  file = " + videofile + "\n";
+                        int numErrors = errors.Split('\n').Length;
+                        labelErrors.Text = numErrors + " errors!";
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("!!!!!!!!!!!!!!!!!1 ERROR!!!!");
+                    Console.WriteLine("success = " + success + "  exists = " + exists + "  size = " + "  error = " + error + "  file = " + videofile);
+                    errors += "success = " + success + "  exists = " + exists + "  size = " + "  error = " + error + "  file = " + videofile + "\n";
+                    int numLines = errors.Split('\n').Length;
+                    labelErrors.Text = numLines + " errors!";
+                }
+            }
+
+            //listBox1.Enabled = true;
+            buttonDelSelected.Enabled = true;
+            buttonGo.Enabled = true;
+            textBoxRequest.Enabled = true;
+            buttonStop.Enabled = false;
+        }
+
+
+
+        #region FILE LIST
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            Console.WriteLine("Processing drop...");
+            foreach (string file in files)
+            {
+                if (file.ToLower().EndsWith(".mp4")
+                    || file.ToLower().EndsWith(".avi")
+                    || file.ToLower().EndsWith(".mov")
+                    || file.ToLower().EndsWith(".mpg")
+                    || file.ToLower().EndsWith(".3gp")
+                    || file.ToLower().EndsWith(".wmv")
+                    || file.ToLower().EndsWith(".m4a")
+                    || file.ToLower().EndsWith(".mkv"))
+                {
+                    Console.WriteLine("File added: " + file);
+                    listBox1.Items.Add(file);
+                }
+                else
+                {
+                    Console.WriteLine("File have unsupported format: " + file);
+                }
+            }
+            Console.WriteLine("Total files: " + listBox1.Items.Count);
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                Console.WriteLine("Detected Drag'n'drop");
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+        private void buttonDelSelected_Click(object sender, EventArgs e)
+        {
+            while (listBox1.SelectedItem != null)
+            {
+                Console.WriteLine("Deleted: " + listBox1.SelectedItem);
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }
+            Console.WriteLine("Total files: " + listBox1.Items.Count);
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listBox1.SelectedItem != null) 
+            {
+                Console.WriteLine("Opening: " + listBox1.SelectedItem);
+                System.Diagnostics.Process.Start(listBox1.SelectedItem.ToString());
+            }
+        }
+
+        #endregion
+
+        #region CMD
+
+
+        private void cmd(string cmd)
+        {
+            Console.WriteLine("Executing command: " + cmd);
+            process = new Process();
+            process.StartInfo.WorkingDirectory = Directory.GetCurrentDirectory() + "\\";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "/c " + cmd;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.OutputDataReceived += process_OutputDataReceived;
+            process.ErrorDataReceived += process_ErrorDataReceived;
+
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            while (process != null && !process.HasExited)
+            {
+                Application.DoEvents();
+                Thread.Sleep(200);
+            }
+           // process.WaitForExit();
+            if(process != null)
+                process.Close();
+            process = null;
+            Console.WriteLine("Command executed.");
+        }
+
+        void process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(e.Data);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+
+        void process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(e.Data);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+
+        #endregion
+
+        private void labelErrors_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(errors);
+        }
+
+        private void labelReports_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(reports);
+        }
+        protected virtual bool IsFileLocked(FileInfo file)
+        {
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None);
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+
+            //file is not locked
+            return false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (process != null)
+            {
+                Console.WriteLine("Stopping...");
+                stop = true;
+                process.Kill();
+                process = null;
+
+                buttonDelSelected.Enabled = true;
+                buttonGo.Enabled = true;
+                textBoxRequest.Enabled = true;
+                buttonStop.Enabled = false;
+            }
+        }
+    }
+}
